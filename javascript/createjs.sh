ef153a8f7283e795465905e0d007f14f111f4769
Angus.C <102298861+Angus1811@users.noreply.github.com>
2023-05-18 08:16:02 +0100

    error handling

--- javascript/createjs.sh
+++ javascript/createjs.sh
@@ -8,6 +8,25 @@ fi
 project_name="$1"
 path="/home/$USER/Documents/code/$project_name"
 
+path="/home/$USER/Documents/code/$project_name"
+
+echo "checking files"
+
+if [ -d "/home/$USER/Documents" ]; then
+    if [ -d "/home/$USER/Documents/code" ]; then
+        if [ -d "$path" ]; then
+            echo "project already exists (please select a diffrent name)"
+            exit 1
+        else
+            continue
+        fi
+    else
+        mkdir "/home/$USER/Documents/code"
+    fi
+else
+    mkdir "/home/$USER/Documents"
+fi
+
 mkdir "$path"
 cd "$path"
 git clone "https://github.com/Angus1811/node-template.git"

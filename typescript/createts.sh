7a2227a5e5dddfd60bd3a6f51d11fc3104a4ea46
Angus1811 <anguscrighton1@gmail.com>
2023-05-19 16:13:07 +0100

    performace  & readability

--- typescript/createts.sh
+++ typescript/createts.sh
@@ -8,17 +8,13 @@ fi
 project_name="$1"
 path="/home/$USER/Documents/code/$project_name"
 
-path="/home/$USER/Documents/code/$project_name"
-
 echo "checking files"
-
 
 #-p is used so that we dont have to do any checks for if the dircotry exists (instead it will create all the directorys required)
 
 mkdir "$path" -p
 cd "$path"
 
-git clone "https://github.com/Angus1811/node-template.git"
-
+git clone "https://github.com/Angus1811/typescript-template.git"
 echo "Created project: $project_name"
 code "$path"

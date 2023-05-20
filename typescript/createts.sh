7bba3fd7c5b3245eda60ed88feb36fa19b447f70
Angus1811 <anguscrighton1@gmail.com>
2023-05-20 15:36:15 +0100

    error handling

--- typescript/createts.sh
+++ typescript/createts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Please provide a project name."
+    echo "ERROR: Please provide a project name."
     exit 1
 fi
 
@@ -9,6 +9,11 @@ project_name="$1"
 path="/home/$USER/Documents/code/$project_name"
 
 echo "checking files"
+
+if [ -d "$path" ]; then
+  echo "ERROR: Project Already Exists."
+  exit 1
+fi
 
 #-p is used so that we dont have to do any checks for if the dircotry exists (instead it will create all the directorys required)
 
